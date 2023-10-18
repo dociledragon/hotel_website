436eb2c7a8a773af6ff1b91d8a1491af5a8e2293
dociledragon <144804286+dociledragon@users.noreply.github.com>
2023-10-18 08:16:37 +0530

    Add files via upload

---
+++ hotel booking/index.php
@@ -0,0 +1,136 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Hotel Booking</title>
+
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+   
+    
+    <style>
+        .well
+        {
+            background: rgba(0,0,0,0.7);
+            border: none;
+    
+        }
+        .wellfix
+        {
+            background: rgba(0,0,0,0.7);
+            border: none;
+            height: 150px;
+        }
+		body
+		{
+			background-image: url('images/home_bg.jpg');
+			background-repeat: no-repeat;
+			background-attachment: fixed;
+		}
+		p
+		{
+			font-size: 13px;
+		}
+        .pro_pic
+        {
+            border-radius: 50%;
+            height: 50px;
+            width: 50px;
+            margin-bottom: 15px;
+            margin-right: 15px;
+        }
+		
+    </style>
+    
+    
+</head>
+
+<body>
+    <div class="container">
+      
+      
+       <img class="img-responsive" src="images/celestia.png" style="width:100%; height:180px;">      
+        <nav class="navbar navbar-inverse">
+            <div class="container-fluid">
+                <ul class="nav navbar-nav">
+                    <li class="active"><a href="index.php">Home</a></li>
+                    <li><a href="room.php">Room &amp; Facilities</a></li>
+                    <li><a href="reservation.php">Online Reservation</a></li>
+                    <li><a href="admin.php">Admin</a></li>
+                </ul>
+                <ul class="nav navbar-nav navbar-right">
+                    <li><a href="http://www.facebook.com"><img src="images/facebook.png"></a></li>
+                    <li><a href="http://www.twitter.com"><img src="images/twitter.png"></a></li>                    
+                </ul>
+            </div>
+        </nav>
+
+     
+        <div class="jumbotron">
+        <div class="w3-content w3-section">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/1.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/2.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/3.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/4.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/5.jpg" style="width:100%; height:450px;">
+            <img class="mySlides w3-animate-fading" src="images/home_gallary/6.jpg" style="width:100%; height:450px;">
+        </div>    
+        </div>
+        <hr>
+        <div class="row" style="color: #ed9e21">
+            <div class="col-md-12 well" >
+              <h4><strong style="color: #ffbb2b">About</strong></h4><br>
+              <p>Welcome to Hotel Celestia Abodes, where comfort meets elegance, and every stay is a celestial journey of luxury and relaxation. Nestled amidst lush landscapes and adorned with modern sophistication, our accommodations offer a haven of tranquility and style.</p>
+              <br>
+              <p>Discover a haven of tranquility at Hotel Celestia Abodes, where impeccable service and breathtaking views elevate your stay to the stars. Enjoy the indulgence of our spacious suites, fine dining at our celestial-themed restaurant, and unwind in our rejuvenating spa—your celestial escape awaits.</p>
+              <br>
+              <p>Experience the celestial charm of Hotel Celestia Abodes, where personalized hospitality and opulent amenities create an unforgettable celestial escape.</p>
+              
+            </div>  
+        </div>
+        <div class="row" style="color: #ffbb2b">
+            <div class="col-md-4 wellfix">
+              <h4><strong>Contact Us</strong></h4><hr>
+              Address : Celestia Abodes, Bibwewadi, Pune<br>
+              Mail : celestiaofficial@gmail.com <br>
+            </div>
+            <div class="col-md-4"></div>
+            <div class="col-md-4 wellfix">
+                <h4><strong>Developed By</strong></h4><hr>
+                <a href="#">Batch 1 Group 2</a>
+
+            </div>
+        </div>
+        
+
+
+
+    </div>
+    
+    
+    
+    
+    
+
+
+
+
+    <script src="my_js/slide.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+
+</html>
